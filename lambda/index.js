50e7aa1a4ee880631eeef18fca819f7d7631c324
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-10-27 10:36:42 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -25,7 +25,7 @@ const HelloWorldIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Hello World!';
+        const speakOutput = 'Hello World! Its wonderful out there !';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
